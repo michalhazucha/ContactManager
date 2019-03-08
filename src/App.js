ee62dd7015e590ae0aec3977e7bd5b71fe21aad9
Michal Hazucha <hazuchamichal@gmail.com>
2019-03-08 20:17:00 +0100

    hashrouter to browserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {HashRouter as Router,Route,Switch} from 'react-router-dom';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import Contacts from './components/contacts/Contacts';
