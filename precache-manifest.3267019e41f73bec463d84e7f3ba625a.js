b626acd94d6b8a1bd6f5a48b3171591f7e4d4bf7
Michal Hazucha <hazuchamichal@gmail.com>
2019-03-08 20:03:14 +0100

    Updates

---
+++ precache-manifest.3267019e41f73bec463d84e7f3ba625a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4117ebe16c2f925170c4",
+    "url": "/ContactManager/static/css/main.c88a039c.chunk.css"
+  },
+  {
+    "revision": "4117ebe16c2f925170c4",
+    "url": "/ContactManager/static/js/main.4117ebe1.chunk.js"
+  },
+  {
+    "revision": "8c2e27197b3338a18e7b",
+    "url": "/ContactManager/static/js/runtime~main.8c2e2719.js"
+  },
+  {
+    "revision": "fbdd18be7f19763019bb",
+    "url": "/ContactManager/static/css/2.e78f769a.chunk.css"
+  },
+  {
+    "revision": "fbdd18be7f19763019bb",
+    "url": "/ContactManager/static/js/2.fbdd18be.chunk.js"
+  },
+  {
+    "revision": "e0210614315766291783d8cadb5947c4",
+    "url": "/ContactManager/index.html"
+  }
+];
