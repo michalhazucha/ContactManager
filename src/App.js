2f278bfd1d617754bcfa7280649504177ef5f323
Michal Hazucha <hazuchamichal@gmail.com>
2019-03-08 20:26:53 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
               <div className="container">
               <Switch>
                 <Route exact path='/' component={Contacts}></Route>
-                <Route exact path='/about' component={About}></Route>
                 <Route exact path='/contact/add' component={AddContact}></Route>
                  <Route exact path='/contact/edit/:id' component={EditContact}></Route>
                 <Route exact path="/test" component={Test}></Route>
